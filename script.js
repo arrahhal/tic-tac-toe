060c398aa1bbf4a149e016810640edb017c7a95c
Abdulrahman Rahhal <114484556+rahhaly@users.noreply.github.com>
2023-03-06 19:00:58 +0300

    Fix: tie result not displaying

--- script.js
+++ script.js
@@ -123,7 +123,7 @@ const GameController = (() => {
     // checks for a tie
     let tieFlag = true;
     for (let i = 0; i < 3; i++)
-      for (let j = 0; j < 3; j++) if (board[i][j] !== 0) tieFlag = false;
+      for (let j = 0; j < 3; j++) if (board[i][j] === 0) tieFlag = false;
     if(tieFlag) return 'tie';
   };
 
